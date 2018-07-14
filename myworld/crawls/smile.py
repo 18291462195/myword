fba5cf02b9d1861f942532c5dc3d2739a7c20f85
gexianyu <1961155818@qq.com>
2018-07-14 13:53:20 +0800

    人挺好

--- myworld/crawls/smile.py
+++ myworld/crawls/smile.py
@@ -10,7 +10,7 @@ from queue import Queue
 from lxml import etree
 import json
 import time
-
+1
 # url = 'https://www.qiushibaike.com/8hr/page/%d/'
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36'}
